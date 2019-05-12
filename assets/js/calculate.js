636d069a24c0f4db9b3a52f690d3674ec0f997fd
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2019-05-12 10:58:34 -0700

    Fixed speed issue

--- assets/js/calculate.js
+++ assets/js/calculate.js
@@ -19,14 +19,6 @@ console.log(total)
 console.log(received/total)
 endPercent = parseInt((received/total)*100)
 
-var color = "bg-success";
-if (endPercent < 90)
-    color = "bg-info";
-if (endPercent < 80)
-    color = "bg-warning";
-if (endPercent > 70)
-    color = "bg-danger";
-console.log(color)
 chrome.storage.local.set({percent: String(endPercent)}, function() {
 });
 chrome.storage.local.set({points: parseFloat(received.toFixed(4))}, function() {
@@ -34,22 +26,13 @@ chrome.storage.local.set({points: parseFloat(received.toFixed(4))}, function() {
 chrome.storage.local.set({points2: parseFloat(total.toFixed(4))}, function(){
 
 });
-// document.getElementsByTagName("header")[0].innerHTML += "<b>" + `
-// <div class="progress">
-//   <div class="progress-bar ` + color + `" style="width:` + String(endPercent) + `%">` + String(endPercent) + `%</div>
-// </div>
-// `
+
 document.getElementsByTagName("header")[0].innerHTML += "<b>" + `
 <div class="progress">
   <div id="dynamic" class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%">
     <span id="current-progress"></span>
   </div>
 </div>`
-
-// $(function() { 
-//     $("#dynamic").addClass("progress-bar-purple");
-//     $("#dynamic").addClass("progress-bar-orange");
-//  });
 
 $(function() {
     var current_progress = 0;
@@ -60,7 +43,6 @@ $(function() {
         .attr("aria-valuenow", current_progress)
         .text(current_progress + "%");
         var attrs = $("#dynamic").attr("class").split(" ")
-        // console.log(attrs);
         if(current_progress < 90)
             attrs[1] = "progress-bar-green";
         if(current_progress < 80)
@@ -76,5 +58,5 @@ $(function() {
 
         if (current_progress >= endPercent)
             clearInterval(interval);
-    }, 50);
+    }, 1);
   });
