d538a5e29de8fade2a4289123e4dee733809d0c5
Salvador aleguas <42396136+saleguas@users.noreply.github.com>
2019-05-11 18:25:45 -0700

    Working commit

---
+++ calculate.js
@@ -0,0 +1,21 @@
+var ak = $("label")
+var received = 0
+var total = 0
+var endPercent = 0
+for(i = 1; i < ak.length; i++)
+{
+    let a = String(ak[i-1].innerText)
+    let b = String(ak[i].innerText)
+    if (a.includes(" / ") && b.includes("%")){
+        let nums = a.split(" / ")
+        if (nums[0] != "-") {
+            received = received + Number(nums[0])
+            total = total + Number(nums[1])
+        }
+    }
+}
+console.log(received)
+console.log(total)
+console.log(received/total)
+endPercent = received/total
+chrome.storage.sync.set('percent', endPercent)
